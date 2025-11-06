7879fbdda2c9897b4e80fd31a35bee7e05e6f3e8
yas1nsyed <yasin.m.syed@fau.de>
2025-11-06 17:52:43 +0100

    debug

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/yas1nsyed.github.io/", // 👈 Add this line
   server: {
     host: "::",
     port: 8080,
