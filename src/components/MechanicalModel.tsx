1b39f9d29f40a7ae80025f896c37c57736f36284
yas1nsyed <yasin.m.syed@fau.de>
2025-11-13 23:31:45 +0100

    trying flange

--- src/components/MechanicalModel.tsx
+++ src/components/MechanicalModel.tsx
@@ -2,6 +2,7 @@ import { useRef, useMemo } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
 import * as THREE from 'three';
+import { CSG } from 'three-csg-ts';
 
 interface FlangeProps {
   position?: [number, number, number];
