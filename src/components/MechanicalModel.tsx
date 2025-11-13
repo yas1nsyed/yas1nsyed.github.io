ed2a4268f536e7e95dafb30b4ad4b4248a582979
yas1nsyed <yasin.m.syed@fau.de>
2025-11-13 23:38:16 +0100

    trying flange

--- src/components/MechanicalModel.tsx
+++ src/components/MechanicalModel.tsx
@@ -148,12 +148,12 @@ const MechanicalModel = () => {
   return (
     <div className="w-full h-[500px] bg-background/50 rounded-xl overflow-hidden border border-primary/20">
       <Canvas shadows>
-        <PerspectiveCamera makeDefault position={[5, 3, 5]} />
+        <PerspectiveCamera makeDefault position={[250, 200, 250]} />
         <OrbitControls 
           enableZoom={true}
           enablePan={true}
-          minDistance={3}
-          maxDistance={15}
+          minDistance={50}
+          maxDistance={600}
           autoRotate
           autoRotateSpeed={0.5}
         />
@@ -175,10 +175,10 @@ const MechanicalModel = () => {
         <Flange position={[0, 0, 0]} rotationSpeed={0.005} />
         
         {/* Grid helper for CAD feel */}
-        <gridHelper args={[10, 10, '#00d9ff', '#334155']} position={[0, -2, 0]} />
+        <gridHelper args={[500, 50, '#00d9ff', '#334155']} position={[0, -50, 0]} />
         
         {/* Axis helper */}
-        <axesHelper args={[3]} />
+        <axesHelper args={[100]} />
       </Canvas>
     </div>
   );
